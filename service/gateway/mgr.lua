223e18eca16d62477508bb0195399c4acd11b097
Hanlei Qin <qinhanlei@gmail.com>
2019-12-20 18:03:02 +0800

    up

--- service/gateway/mgr.lua
+++ service/gateway/mgr.lua
@@ -1,18 +1,47 @@
 local skynet = require "skynet"
+require "skynet.manager"
+local socket = require "skynet.socket"
 local log = require "tm.log"
+local thread = tonumber(skynet.getenv("thread"))
+
+local agents
+
+local ip = "127.0.0.1"
+local port = 10086
+
+
+local function pickagent(id)
+	log.debug("pick %d ws agent for wsid:%d", (id % #agents) + 1, id)
+	return agents[(id % #agents) + 1]
+end
+
+
+local function launch()
+	agents = {}
+	for i = 1, thread do
+		log.debug("launch gateway/agent [%d]", i)
+		agents[i] = skynet.newservice("gateway/agent", i)
+	end
+	log.debug("listen on %s:%s", ip, port)
+	local listenid = socket.listen(ip, port)
+	socket.start(listenid, function(id, addr)
+		log.debug("on connect id:%s addr:%s", id, addr)
+		skynet.send(pickagent(id), "lua", "start", id, addr)
+	end)
+end
+
 
 local CMD = {}
 
-skynet.start(function()
-	skynet.dispatch("lua", function(session, source, cmd, ...)
-		local f = CMD[cmd]
-		if f then
-			skynet.retpack(f(...))
-		else
-			log.error("from[%s:%s] cmd:%s not found", session, source, cmd)
-		end
-	end)
+function CMD.pickagent(wsid)
+	return pickagent(wsid)
+end
 
-	local gated = skynet.uniqueservice("gateway/gated")
-	skynet.send(gated, "lua", "start", "127.0.0.1", 10086)
+
+skynet.start(function()
+	skynet.dispatch("lua", function(_, _, cmd, ...)
+		local f = assert(CMD[cmd], cmd .. " not found")
+		skynet.retpack(f(...))
+	end)
+	skynet.fork(launch)
 end)
